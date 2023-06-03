fb6f4b98044ca04545cbea12ed9fecdc9755e71b
Manuel <81485512+ManuelsKopf@users.noreply.github.com>
2023-06-03 13:21:47 +0200

    inserted website

---
+++ webSite.py
@@ -0,0 +1,19 @@
+import streamlit as st
+# st.text('Fixed width text')
+# st.markdown('_Markdown_') # see *
+# st.latex(r''' e^{i\pi} + 1 = 0 ''')
+# st.write('Most objects') # df, err, func, keras!
+# st.write(['st', 'is <', 3]) # see *
+st.title('Summary')
+st.header('Data of the software: ')
+st.subheader('Publication date: ')
+st.subheader('Kind of Software: ')
+st.subheader('Number of evaluation:')
+st.header('Data of the company: ')
+st.subheader('Name of the company:')
+st.subheader('Number of employees:')
+st.header('Summary')
+st.text('Fixed width text')
+st.json({'foo':'bar','fu':'ba'})
+st.header('Number of Stars')
+
