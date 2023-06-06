1a2f503ed3e73464efa380b68ea11cc0b923fb21
Max Finkenbeiner <48648552+Runlay@users.noreply.github.com>
2023-06-06 16:23:03 +0200

    Update webSite.py

--- webSite.py
+++ webSite.py
@@ -5,11 +5,14 @@ import time
 
 st.title('Trustpilot Summary Generator')
 
+
+
 col1, col2 = st.columns(2)
 
 url_input=col1.text_input('Enter 1st Trustpilot URL: ')
 url_input2=col2.text_input('Enter 2nd Trustpilot URL: ')
-if url_input:
+
+with col1:
     st.write('Crawling: ', url_input)
 
     crawler.createSumSent(url_input)
@@ -21,20 +24,43 @@ if url_input:
     jsonObject2 = json.load(open('sentiment.json'))
     col1.json(jsonObject2)
 
-    # st.header('Number of Stars')
+with col2:
+    st.write('Crawling: ', url_input2)
 
-if url_input2:
-    st.write('Crawling: ', url_input)
-
-    crawler.createSumSent(url_input)
+    crawler.createSumSent(url_input2)
 
     # time.sleep(10)
     # st.text('Fixed width text')
-    jsonObject = json.load(open('summary.json'))
-    col2.json(jsonObject)
-    jsonObject2 = json.load(open('sentiment.json'))
-    col2.json(jsonObject2)
+    jsonObject3 = json.load(open('summary.json'))
+    col2.json(jsonObject3)
+    jsonObject4 = json.load(open('sentiment.json'))
+    col2.json(jsonObject4)
+# if url_input:
+#     st.write('Crawling: ', url_input)
 
-    # st.header('Number of Stars')
+#     crawler.createSumSent(url_input)
+
+#     # time.sleep(10)
+#     # st.text('Fixed width text')
+#     jsonObject = json.load(open('summary.json'))
+#     col1.json(jsonObject)
+#     jsonObject2 = json.load(open('sentiment.json'))
+#     col1.json(jsonObject2)
+
+#     # st.header('Number of Stars')
+
+# if url_input2:
+#     st.write('Crawling: ', url_input)
+
+#     crawler.createSumSent(url_input)
+
+#     # time.sleep(10)
+#     # st.text('Fixed width text')
+#     jsonObject = json.load(open('summary.json'))
+#     col2.json(jsonObject)
+#     jsonObject2 = json.load(open('sentiment.json'))
+#     col2.json(jsonObject2)
+
+#     # st.header('Number of Stars')
 
 
