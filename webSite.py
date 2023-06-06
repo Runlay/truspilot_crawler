bcc61a517da74a5d712512da7256c818c2a25797
Max Finkenbeiner <48648552+Runlay@users.noreply.github.com>
2023-06-06 14:05:19 +0200

    Update webSite.py

--- webSite.py
+++ webSite.py
@@ -6,20 +6,20 @@ import json
 # st.write('Most objects') # df, err, func, keras!
 # st.write(['st', 'is <', 3]) # see *
 st.title('Summary')
-st.header('Data of the software: ')
-st.subheader('Publication date: ')
-st.subheader('Kind of Software: ')
-st.subheader('Number of evaluation:')
-st.header('Data of the company: ')
-st.subheader('Name of the company:')
-st.subheader('Number of employees:')
-st.header('Summary')
+# st.header('Data of the software: ')
+# st.subheader('Publication date: ')
+# st.subheader('Kind of Software: ')
+# st.subheader('Number of evaluation:')
+# st.header('Data of the company: ')
+# st.subheader('Name of the company:')
+# st.subheader('Number of employees:')
+# st.header('Summary')
 
 
-st.text('Fixed width text')
-jsonObject = json.load('summary.json')
+# st.text('Fixed width text')
+jsonObject = json.load(open('summary.json'))
 st.json(jsonObject)
-jsonObject2 = json.load('sentiment.json')
+jsonObject2 = json.load(open('sentiment.json'))
 st.json(jsonObject2)
 # st.header('Number of Stars')
 
